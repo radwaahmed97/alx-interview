06b3465a002c2efe359f276eff94a1f1ba6a33e6
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-15 16:56:54 -0400

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,46 @@
 #!/usr/bin/env python3
 """reads stdin line by line and computes metrics"""
 
-import random
 import sys
-import time
-import datetime
-
-
-def print_stats(total_file_size, status_codes):
-    """Prints the statistics"""
-    print("File size: {}".format(total_file_size))
-    for key, value in sorted(status_codes.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
 
 
 total_file_size = 0
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
 counter = 0
-code = 0
+
 try:
     for line in sys.stdin:
-        parsed_state = line.split()
-        parsed_state = parsed_state[::-1]
+        parsed_state = line.split(" ")
         if len(parsed_state) > 2:
+            status_line = parsed_state[-2]
+            file_size = parsed_state[-1]
+            if status_line in status_codes:
+                status_codes[status_line] += 1
+            total_file_size += int(file_size)
             counter += 1
-            if counter <= 10:
-                total_file_size += int(parsed_state[0])
-                code = parsed_state[1]
-                if (code in status_codes.keys()):
-                    status_codes[code] += 1
-
             if counter == 10:
-                print_stats(total_file_size, status_codes)
+                print("File size: {:d}".format(total_file_size))
+                keys_sorted = sorted(status_codes.keys())
+                for key in keys_sorted:
+                    val = status_codes[key]
+                    if val != 0:
+                        print("{}: {}".format(key, val))
                 counter = 0
+except Exception:
+    pass
 finally:
-    print_stats(total_file_size, status_codes)
+    print("File size: {:d}".format(total_file_size))
+    keys_sorted = sorted(status_codes.keys())
+    for key in keys_sorted:
+        val = status_codes[key]
+        if val != 0:
+            print("{}: {}".format(key, val))
