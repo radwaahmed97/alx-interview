94a9a30f50992454df5d4b1b0542734a356277ac
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-22 19:47:10 -0400

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,11 +1,28 @@
-#!/usr/bin/python3
-"""method determines if given data set represents a valid UTF-8 encoding"""
 from typing import List
 
 
 def validUTF8(data: List[int]) -> bool:
-    """returns True if data is valid UTF-8 encoding else return False"""
-    for i in range(len(data)):
-        if data[i] in range(-128, 126):
-            return True
-        return False
+    """Returns True if data is valid UTF-8 encoding, else False"""
+    num_bytes = 0
+
+    for byte in data:
+        if byte < 0 or byte > 255:
+            return False
+
+        if num_bytes == 0:
+            if (byte & 0b10000000) == 0:
+                continue
+            elif (byte & 0b11100000) == 0b11000000:
+                num_bytes = 1
+            elif (byte & 0b11110000) == 0b11100000:
+                num_bytes = 2
+            elif (byte & 0b11111000) == 0b11110000:
+                num_bytes = 3
+            else:
+                return False
+        else:
+            if (byte & 0b11000000) != 0b10000000:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
