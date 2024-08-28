2dc21b03488e0f2298764f7e7467ea5227ad9cea
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-28 14:22:18 -0400

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -32,6 +32,16 @@ def queens_recursion(N):
     return result
 
 
+def args_eligibility(N):
+    """check if args are eligible"""
+    if not N.isdigit():
+        print("N must be a number")
+        exit(1)
+    if int(N) < 4:
+        print("N must be at least 4")
+        exit(1)
+
+
 def solve():
     """
     program solves the challenge of placing N non-attacking queens
@@ -40,18 +50,11 @@ def solve():
     if len(sys.argv) != 2:
         print("Usage: nqueens N\n")
         sys.exit(1)
-    else:
-        N = sys.argv[1]
-        if not N.isdigit():
-            print("N must be a number")
-            exit(1)
-        elif int(N) < 4:
-            print("N must be at least 4\n")
-            sys.exit(1)
-        else:
-            possible_sols = queens_recursion(int(N))
-            for sol in possible_sols:
-                print(sol)
+    N = sys.argv[1]
+    args_eligibility(N)
+    possible_sols = queens_recursion(int(N))
+    for sol in possible_sols:
+        print(sol)
 
 
 if __name__ == '__main__':
