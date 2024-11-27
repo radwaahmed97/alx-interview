782b3f175b71bd7ea4ed292aaa1efd7ff1adf625
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-11-26 22:32:18 -0500

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""
-Given a pile of coins of different values,
-determine the fewest number of coins
-that needed to meet a given amount total
-"""
+
 from math import floor
 
 
 def makeChange(coins, total):
-    """Returns fewest number of coins needed to meet total"""
     if total <= 0:
         return 0
-    else:
-        coins_sum = 0
-        coins.sort(reverse=True)
-        for i in range(len(coins)):
-            coins_sum += coins[i]
-            if coins_sum == total:
-                return i + 1
-            elif coins_sum < total:
-                if total % coins_sum == 0:
-                    return total // coins_sum
-                else:
-                    n_coins_i = floor(total / coins_sum)
-                    num_coins = n_coins_i + ((total % coins_sum)/coins[i+1])
-                    if (total % coins_sum) % coins[i+1] == 0:
-                        return int(num_coins)
-                    else:
-                        return -1
+    coins.sort(reverse=True)
+    coin_counter = 0
+    
+    for coin in coins:
+        if total == 0:
+            break
+        if coin <= total:
+            count = total // coin
+            coin_counter += count
+            total -= count * coin
+    
+    if total > 0:
+        return -1
+    
+    return coin_counter
